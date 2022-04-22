2c965f124176ec945e1ff7e2ccc7f492644a1388
Anzo52 <azollner2014@gmail.com>
2022-04-22 12:45:57 -0600

    Create flip_and_guess.py

---
+++ flip_and_guess.py
@@ -0,0 +1,41 @@
+import datetime
+import random
+import schedule
+import time
+from __future__ import print_function
+from random import randint, choice
+
+
+score = 0
+coin_flip = 0
+coin_guess = 0
+dt = datetime.datetime.now().time()
+
+def flip():
+  for _ in range(n):
+    k = random.randint(0, 1)
+    coin_flip = k
+    
+def guess():
+  for _ in range(n):
+    k = random.randint(0,1)
+    coin_guess = k
+  
+def score():
+  if (abs(coin_flip - coin_guess)) > 0:
+    score -= 1
+  else:
+    score += 1
+  if (score < 0):
+    print(dt + ' -' + score)
+  elif (score == 0):
+    print(dt + ' ' + score)
+  else:
+    print(dt + ' +' + score)
+
+schedule.every(1).minutes.do(flip)
+schedule.every(1).minutes.do(guess)
+schedule.every(1).minutes.do(score)
+while(true):
+  schedule.run_pending()
+  time.sleep(1)
